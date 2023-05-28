009e642e051db38efaa09c1d5344f4a348fe7a62
owx0130 <weixiang0130@gmail.com>
2023-05-29 00:14:19 +0800

    Add Home Page Pic

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
+      <img src="https://drive.google.com/uc?export=view&id=1A7UTYDRycQ-mZObRH-O-npLcZ_IyhUuK" />
     </>
   )
 }
