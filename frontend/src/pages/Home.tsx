e417d66ffcfa81e05a660e11dc114943b20fa3d7
owx0130 <weixiang0130@gmail.com>
2023-05-29 00:19:32 +0800

    Add Home Page Pic

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <img src="https://drive.google.com/uc?export=view&id=1A7UTYDRycQ-mZObRH-O-npLcZ_IyhUuK" />
+      <div className='row'>
+        <div className='col'></div>
+        <img className='col w-50' src="https://drive.google.com/uc?export=view&id=1A7UTYDRycQ-mZObRH-O-npLcZ_IyhUuK" />
+        <div className='col'></div>
+      </div>
     </>
   )
 }
