6a50c4ce66af1a18012dbc046781a9e59ad1b142
Yeo Zong Yao <78098867+yeozongyao@users.noreply.github.com>
2023-06-25 05:55:38 +0800

    Update views.py

--- api/views.py
+++ api/views.py
@@ -20,11 +20,19 @@ def add_item(request):
   
   return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+
 @api_view(['PUT'])
 def edit_item(request):
-  serializer = BracketSerializer(data=request.data)
-  if serializer.is_valid():
-    serializer.save()
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
-  
-  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        instance = Bracket.objects.get(id=request.data['id'])
+    except Bracket.DoesNotExist:
+        return Response({"error": "Bracket not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = BracketSerializer(instance, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
