9c5d5d096f5f840164d347b63668e5461b80e14b
Yeo Zong Yao <78098867+yeozongyao@users.noreply.github.com>
2023-06-25 10:26:10 +0800

    Update views.py

--- users/views.py
+++ users/views.py
@@ -3,6 +3,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+
 from .serializers import *
 from .models import Bracket
 
@@ -15,13 +20,21 @@ def get_data(request):
 
 
 @api_view(['POST'])
-def add_item(request):
-    serializer = BracketSerializer(data=request.data)
+def RegisterUser(request):
+    serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        User.objects.create_user(serializer.data['username'], serializer.data['email'], serializer.data['password'])
+        return Response(None, status=status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def LoginUser(request):
+    user = authenticate(username=request.data['username'], password=request.data['password'])
+    if user is not None:
+        return Response(None, status=status.HTTP_201_CREATED)
+
+    return Response(None, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['PUT'])
