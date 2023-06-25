feeff41c62fc33a1da8c81faeed41d0a380743e4
owx0130 <weixiang0130@gmail.com>
2023-06-25 23:53:09 +0800

    change poster link

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="row">
         <span className="text-center fs-3 fw-bold py-3">Welcome to nusscores.com!</span>
         <div className="col"></div>
-        <img className="col w-50" src="https://drive.google.com/uc?export=view&id=1cktIs0EIhcBLp9ws3pxcYAqOdKMSYQFv" />
+        <img className="col w-50" src="https://drive.google.com/uc?export=view&id=1g9ClWzMOiSrjj_-x73Jv_NCz-to5myGa" />
         <div className="col"></div>
       </div>
     </>
